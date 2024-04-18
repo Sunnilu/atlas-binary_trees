67d0b07d01b5c5e94b4bec2656e7b04088b49e63
Suntha <Suntha.Lucas@atlasschool.com>
2024-04-18 21:17:41 +0000

    update to 15-binary

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -19,4 +19,15 @@ return (0);
 }
 return (1);
 }
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: a pointer to the root node of the tree to check
+ * return: if tree is NULL or is not full - 0, otherwise -1
+ */
 
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (is_full_recursive(tree));
+}
